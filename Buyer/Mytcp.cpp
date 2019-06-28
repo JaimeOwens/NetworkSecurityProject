49e1cf0ad2e601fcc787ef073baf2d6a73024d0c
GEN <956441445@qq.com>
2019-06-28 18:25:10 +0800

    add tcp client

--- Buyer/Mytcp.cpp
+++ Buyer/Mytcp.cpp
@@ -1,13 +1,18 @@
-#include "Mytcp.h"
-
-
-
-Mytcp::Mytcp()
-{
-	
+#include"Mytcp.h"
+mytcp::mytcp(std::string add,int port){
+    this->sockfd = socket(AF_INET,SOCK_STREAM,0);
+    struct sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(port);
+    addr.sin_addr.s_addr = inet_addr(add.data());
+    connect(this->sockfd,(struct sockaddr*)&addr,sizeof(addr));
 }
-
-
-Mytcp::~Mytcp()
-{
+int mytcp::mysend(std::string msg){
+    return send(this->sockfd,msg.data(),msg.size(),0);
+}
+int mytcp::myrecv(unsigned char* msg,int n){
+    return recv(this->sockfd,msg,4096,0);
+}
+mytcp::~mytcp(){
+    close(this->sockfd);
 }
--- Buyer/Mytcp.h
+++ Buyer/Mytcp.h
@@ -1,10 +1,21 @@
-#pragma once
-class Mytcp
-{
-private:
-	int sockfd;
-public:
-	Mytcp();
-	~Mytcp();
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include<string>
+#include<iostream>
+class mytcp{
+    private:
+        int sockfd;
+    public:
+        mytcp(std::string add,int port);
+        ~mytcp();
+        int mysend(std::string msg);
+        int myrecv(unsigned char* msg,int n);
 };
-
