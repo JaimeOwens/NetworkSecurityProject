bd54a3879d41d2343f653dde4a69484c7f8d28a7
GEN <956441445@qq.com>
2019-06-29 18:01:44 +0800

    debug
    发送消息中添加ds长度，调整check_msg中的内存分配

--- Buyer/Msg.h
+++ Buyer/Msg.h
@@ -1,22 +1,21 @@
 #pragma once
-#include"../RSA/rsa.h"
-#include"../sha-512/SHA512.h"
-#include"../rc4/rc4.h"
+#include"rsa.h"
+#include"SHA512.h"
+#include"rc4.h"
 #include<string>
-class RSA;
-class SHA512;
 class Msg
 {
 private:
 	std::string pi;
 	std::string oi;
 	char ks[4096];
-	int ds[16];
+	int* ds;
+	int dslen;
 	RSA kr;
 public:
 	Msg();
 	Msg(std::string ks,std::string pi, std::string oi,int n,int e,int d);
-	unsigned char* makemsg(int a ,int b,int &lens);
+	unsigned char* makemsg(int a ,int b,int& lens);
 	~Msg();
 };
 
