3b1a58c1c8e88ce7ede6e5e7c194b6b7dadca656
GEN <956441445@qq.com>
2019-06-30 12:42:17 +0800

    delete .o
    求hash的方法参照 main.cpp中的示例

--- sha-512/main.cpp
+++ sha-512/main.cpp
@@ -1,13 +1,8 @@
 #pragma once
 #include"SHA512.h"
 int main() {
-	SHA512 T;
 	char t[] = "1asdfafwafhashfahuidhfiuahsiuvhaiufhiaufhaudhfaosufhoa";
-	hashval hashbuff;
-	hashbuff = T.hash(t);
-	printhash(hashbuff);
-	char t2[] = "1asdfafwafhashfahuidhfiuahsiuvhaiufhiaufhaudhfaosufho4";
-	hashval h2 = T.hash(t2);
-	printhash(h2);
+	std::string hash = hash512(t);
+	hash = hash512(t,strlen(t));
 	return 0;
 }
