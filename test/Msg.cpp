f959fb011372993bf2d804c8bd73134ced31ef21
GEN <956441445@qq.com>
2019-07-02 11:47:25 +0800

    change msg send
    调整了消息发送各部分长度
    如message_frame所示

--- test/Msg.cpp
+++ test/Msg.cpp
@@ -1,7 +1,4 @@
 #include "Msg.h"
-#define SHORTLENGTH 128
-#define MIDLENGTH 256
-#define LONGLENGTH 512
 Msg::Msg()
 {
 
@@ -15,20 +12,17 @@ Msg::Msg(std::string ks,std::string pi, std::string oi,int n,int e,int d)
 	strcpy(this->ks, ks.data());
 	std::string pimd = sha512(pi);
     std::string oimd = sha512(oi);
-	char temp[64];
-	for (int i = 0; i < 64; i++) {
+	char temp[128];
+	for (int i = 0; i < 128; i++) {
 		temp[i] = pimd[i]|oimd[i];
 	}
-	std::string pomd = sha512(temp,64);
-	  unsigned short tds[32];
-    memcpy(tds,pomd.data(),64);
-    int * tempds = this->kr.encrypt((char*)tds);
-    this->dslen = this->kr.GetLength(tempds);
-	//this->dslen = kr.GetCount()*sizeof(int);
-	this->ds = new int[this->dslen];
-	//std::cout<<"dslen "<<this->dslen<<std::endl;
-	memcpy(this->ds,tempds,dslen);
-
+	std::string pomd = sha512(temp,128);
+	  unsigned short tds[64];
+    memcpy(tds,pomd.data(),128);
+	this->ds = this->kr.encrypt((char*)tds);
+	this->dslen = this->kr.GetLength(this->ds);
+	//this->ds = new int[this->dslen/4];
+	//memcpy(this->ds,tempds,dslen);
 }
 
 unsigned char* Msg::makemsg(int kbn, int kbe,int&lens)
@@ -42,48 +36,35 @@ unsigned char* Msg::makemsg(int kbn, int kbe,int&lens)
 	
 	std::string temphs = sha512(this->oi.data());
 	memcpy(MSG_A + this->pi.size() + this->dslen, temphs.data(), 64);
-	std::cout<<"msg_a has ready\n";
 	rc4 rc;
 	MSG_A[A_len] = '\0';
 	char s[256] = { 0 };
 	rc.rc4_setup((unsigned char *)s, (unsigned char*)this->ks, strlen(this->ks));
 	rc.rc4_encrypt((unsigned char*)s, MSG_A, A_len);
-	std::cout<<"rc4 encrypt is ready\n";
 	RSA kb;
 	kb.SetPublicKey(kbn, kbe);
 	int* kstemp = kb.encrypt(this->ks);
-	// kb.decrypt(kstemp);
-	// std::cout<<"de is ready\n";
+	
 	int len = kb.GetLength(kstemp);
-	lens = 12+len+A_len+520;
-
+	lens = 12+len+A_len+768+8;
 	unsigned char *msg = new unsigned char[lens+1];
 	msg[lens] = '\0';
 	memset(msg, 0, lens);
-	
 	memcpy(msg, &A_len, 4);//len a
 	memcpy(msg + 4, &len, 4);//len b
-	memcpy(msg + 8, &(this->dslen), 4);//len ds
-	cout<<A_len<<' '<<len<<' '<<this->dslen<<endl;
-	//std::cout<<"the a_len and b_len is "<<A_len<<" "<<len<<std::endl;
-	memcpy(msg + 12, MSG_A, A_len);
-	memcpy(msg + 12 + A_len, kstemp, len);
-	temphs = sha512(this->pi.data());
-	memcpy(msg + 12 + A_len + len, temphs.data(), temphs.size());
-	memcpy(msg + 12 + A_len + len + MIDLENGTH, this->oi.data(), 64);
-	memcpy(msg + 12 + A_len + len + SHORTLENGTH + MIDLENGTH, this->ds, this->dslen + sizeof(int));
-	memcpy((msg + 12 + A_len + len + SHORTLENGTH + MIDLENGTH + LONGLENGTH), this->kr.GetPublicKey(), 8);
-	cout<<endl;
-	cout<<"MSG_A: "<<MSG_A<<endl;
-	cout<<"MSG_B: "<<kstemp<<endl;
-	cout<<"PIMD: "<<temphs.data()<<endl;
-	cout<<"OI: "<<this->oi.data()<<endl;
-	cout<<"PIMD(length): "<<temphs.size()<<endl;
-	cout<<"DS(length): "<<this->dslen<<endl;
-	cout<<"DS: "<<this->ds<<endl;
+	memcpy(msg + 8,&(this->dslen),4);//len ds
+	memcpy(msg + 12, MSG_A, A_len);//msg_a 
+	memcpy(msg + 12 + A_len, kstemp, len);//ks
+	std::string ttemphs = sha512(this->pi.data());
+	memcpy(msg + 12 + A_len + len, ttemphs.c_str(), 128);//pimd
+	memcpy(msg + 12 + A_len + len + 128, this->oi.data(), this->oi.size());//oi 
+	msg[12+A_len+len+128+this->oi.size()]='\0';
+	memcpy(msg + 12 + A_len + len + 256, this->ds, this->dslen);//DS 512
+
+	memcpy((msg + 12 + A_len + len +768 ), this->kr.GetPublicKey(), 8);
 	return msg;
 }
 Msg::~Msg()
 {
-	delete []this->ds;
+//	delete []this->ds;
 }
--- test/main.cpp
+++ test/main.cpp
@@ -1,6 +1,20 @@
 #include"Msg.h"
 #include"Mytcp.h"
+#define SHORTLENGTH 128
+#define MIDLENGTH 256
+#define LONGLENGTH 512
+#define START 8
+#define _RECVPORT_ 8886
+#define _SENDPORT_ 8888
+#define _BACKLOG_ 10
+struct check_msg{
+	char PIMD[MIDLENGTH];
+	char OI[SHORTLENGTH];
+	char DS[LONGLENGTH];
+	int CC[2];
+};
 int main() {
+	check_msg tmsg;
 	std::string ks, oi, pi;
 	int n, e, d;
 	std::cin >> ks >> oi >> pi >> n >> e >> d;
@@ -8,18 +22,29 @@ int main() {
 	unsigned char* temp = NULL;
 	int lens;
 	temp = m.makemsg(1823347, 549,lens);
+	std::cout<<"the whole len is "<<lens<<std::endl;
 	int a=5, b=6,c=5;
 	if (temp != NULL) {
-		memcpy(&a, temp+lens-8, 4);
-		memcpy(&b, temp +lens-4, 4);
+		memcpy(&a, temp, 4);
+		memcpy(&b, temp +4, 4);
+		memcpy(&c,temp+8,4);
 	}
 	std::cout << a << " " << b <<" "<<c<<std::endl;
-	std::cout<<(char*)(temp+12)<<std::endl;
-	int * te = new int[c];
-	//memcpy(temp+12+)
-	mytcp se("127.0.0.1",8886);
-	std::cout<<se.mysend(temp,lens);
-	//int * key = new int[b/4];
-//	memcpy(temp+8+a,key,b);
+	unsigned char * msga = new unsigned char[a+1];
+	int * msgb = new int[b/4];
+	memcpy(msga,temp+12,a);
+	memcpy(msgb,temp+12+a,b);
+	memcpy(tmsg.PIMD,temp+12+a+b,128);
+	memcpy(tmsg.OI,temp+12+a+b+128,128);
+	memcpy(tmsg.DS,temp+12+a+b+256,c);
+	memcpy(tmsg.CC,temp+12+a+b+768,8);
+	std::cout<<"the OI is    "<<tmsg.OI<<std::endl;
+	std::cout<<tmsg.CC[0]<<" "<<tmsg.CC[1]<<std::endl;
+	RSA ts;
+	ts.SetPublicKey(1823347,549);
+	ts.SetPrivateKey(1823347,990349);
+	ts.decrypt(msgb);
+	std::string ksstr = ts.GetDecoded();
+	std::cout<<"ks is   "<<ksstr<<std::endl;
 	return 0;
 }
